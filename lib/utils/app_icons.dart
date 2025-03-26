d76be956f65e8f258291ebccbd3f718bf6e9ec7e
KhaledElalfy1 <khaledalfy53@gmail.com>
2025-03-26 13:35:00 +0200

    finish light splash Screen

---
+++ lib/utils/app_icons.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: prefer_single_quotes
+class AppIcons {
+  AppIcons._();
+  
+  /// Assets for svgsAuthBackIcon
+  /// assets/svgs/auth_back_icon.svg
+  static const String svgsAuthBackIcon = "assets/svgs/auth_back_icon.svg";
+
+  /// Assets for svgsAuthIcon
+  /// assets/svgs/auth_icon.svg
+  static const String svgsAuthIcon = "assets/svgs/auth_icon.svg";
+
+  /// Assets for svgsEmailCheckedIcon
+  /// assets/svgs/email_checked_icon.svg
+  static const String svgsEmailCheckedIcon = "assets/svgs/email_checked_icon.svg";
+
+  /// Assets for svgsEmailIcon
+  /// assets/svgs/email_icon.svg
+  static const String svgsEmailIcon = "assets/svgs/email_icon.svg";
+
+  /// Assets for svgsOnboardingIcon
+  /// assets/svgs/onboarding_icon.svg
+  static const String svgsOnboardingIcon = "assets/svgs/onboarding_icon.svg";
+
+  /// Assets for svgsSplashIcon
+  /// assets/svgs/splash_icon.svg
+  static const String svgsSplashIcon = "assets/svgs/splash_icon.svg";
+}
+
