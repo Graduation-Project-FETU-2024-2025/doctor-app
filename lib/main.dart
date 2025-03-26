46d4576a1ec365b5b64b65c49928c675d5080777
KhaledElalfy1 <khaledalfy53@gmail.com>
2025-03-26 13:08:48 +0200

    update compileSdk version to 35, initialize CacheHelper, and remove geolocator plugin references

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:doctor_app/core/database/cache/cashe_helper.dart';
 import 'package:doctor_app/core/helpers/bloc_observer.dart';
 import 'package:doctor_app/core/services/get_it.dart';
 import 'package:doctor_app/doctor_app.dart';
@@ -5,7 +6,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = MyBlocObserver();
   setup();
+  await getIt<CacheHelper>().init();
   runApp(const DoctorApp());
 }
