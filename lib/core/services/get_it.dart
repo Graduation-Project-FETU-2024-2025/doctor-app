0b5f74f0115629cce85975f5707b8a8354510f2f
KhaledElalfy1 <khaledalfy53@gmail.com>
2025-03-26 10:20:37 +0200

    add app themes to app add api files and local storage files

--- lib/core/services/get_it.dart
+++ lib/core/services/get_it.dart
@@ -1,6 +1,22 @@
+import 'package:doctor_app/core/database/api/api_consumer.dart';
+import 'package:doctor_app/core/database/api/dio_consumer.dart';
+import 'package:doctor_app/core/database/api/dio_factory.dart';
+import 'package:doctor_app/core/database/cache/cashe_helper.dart';
 import 'package:get_it/get_it.dart';
 
 final getIt = GetIt.instance;
 
 void setup() {
+
+  /// CacheHelper instance
+  getIt.registerLazySingleton<CacheHelper>(
+    () => CacheHelper(),
+  );
+
+  // api data
+
+  getIt.registerLazySingleton<ApiConsumer>(
+    () => DioConsumer(dio: DioFactory.getDio()),
+  );
+
 }
--- lib/doctor_app.dart
+++ lib/doctor_app.dart
@@ -1,32 +1,35 @@
 import 'package:doctor_app/core/routers/app_router.dart';
 import 'package:doctor_app/core/routers/routing.dart';
+import 'package:doctor_app/core/themes/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'generated/l10n.dart';
+
 class DoctorApp extends StatelessWidget {
   const DoctorApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: const Size(393, 852),
-      builder: (_,child) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          localizationsDelegates: [
-                S.delegate,
-                GlobalMaterialLocalizations.delegate,
-                GlobalWidgetsLocalizations.delegate,
-                GlobalCupertinoLocalizations.delegate,
+        designSize: const Size(393, 852),
+        builder: (_, child) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            localizationsDelegates: [
+              S.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
             ],
             supportedLocales: S.delegate.supportedLocales,
             locale: const Locale('en'),
-
-          onGenerateRoute: AppRouter.generateRoute,
-          initialRoute: Routing.splash,
-        );
-      }
-    );
+            theme: Themes.lightTheme,
+            darkTheme: Themes.darkTheme,
+            themeMode: ThemeMode.light,
+            onGenerateRoute: AppRouter.generateRoute,
+            initialRoute: Routing.splash,
+          );
+        });
   }
 }
