a9fda2b073705c11be98262cc1c8d7b654ad2a92
KhaledElalfy1 <khaledalfy53@gmail.com>
2025-04-23 10:40:20 +0200

    add total income section

--- lib/features/dashboard/presentation/view/dashboard_view.dart
+++ lib/features/dashboard/presentation/view/dashboard_view.dart
@@ -1,6 +1,7 @@
 import 'package:doctor_app/core/utils/app_icons.dart';
 import 'package:doctor_app/core/utils/app_styles.dart';
 import 'package:doctor_app/features/dashboard/presentation/view/widgets/booking_section.dart';
+import 'package:doctor_app/features/dashboard/presentation/view/widgets/total_income_section.dart';
 import 'package:doctor_app/features/dashboard/presentation/view/widgets/user_welcome_section.dart';
 import 'package:doctor_app/features/dashboard/presentation/view/widgets/workflow_chart_section.dart';
 import 'package:doctor_app/generated/l10n.dart';
@@ -38,6 +39,8 @@ class DashboardView extends StatelessWidget {
             style: AppStyles.semiBold18(context),
           ),
           BookingSection(),
+          Gap(60.h),
+          const TotalIncomeSection(),
         ],
       ),
     );
---
+++ lib/features/dashboard/presentation/view/widgets/total_income_section.dart
@@ -0,0 +1,37 @@
+import 'package:doctor_app/core/utils/app_colors.dart';
+import 'package:doctor_app/core/utils/app_styles.dart';
+import 'package:doctor_app/generated/l10n.dart';
+import 'package:flutter/widgets.dart';
+import 'package:gap/gap.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class TotalIncomeSection extends StatelessWidget {
+  const TotalIncomeSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CircularPercentIndicator(
+      animation: true,
+      radius: 100.0,
+      lineWidth: 20.0,
+      percent: 0.75,
+      backgroundColor: Color(0xffD9E1F9),
+      center: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            S.of(context).totalIncome,
+            style: AppStyles.semiBold20(context),
+          ),
+          Gap(20.h),
+          Text(
+            '\$122,330,000',
+            style: AppStyles.semiBold18(context),
+          ),
+        ],
+      ),
+      progressColor: AppColors.primaryColor,
+    );
+  }
+}
