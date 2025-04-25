0be827c156a6d041010661d340a7ea8236adef68
Dina Ahmed <denaahmed340@gmail.com>
2025-04-25 15:43:19 +0300

    adjust direction of arrow

--- lib/features/clinic_edit/presentation/view/clinic_edit_view.dart
+++ lib/features/clinic_edit/presentation/view/clinic_edit_view.dart
@@ -2,6 +2,9 @@ import 'package:doctor_app/core/helpers/extentions.dart';
 import 'package:doctor_app/features/clinic_edit/presentation/view/widget/clinic_edit_view_body.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import '../../../../core/database/cache/cache_keys.dart';
+import '../../../../core/database/cache/cashe_helper.dart';
+import '../../../../core/services/get_it.dart';
 import '../../../../core/utils/app_icons.dart';
 
 class ClinicEditView extends StatelessWidget {
@@ -14,7 +17,9 @@ class ClinicEditView extends StatelessWidget {
         backgroundColor: Colors.transparent,
         leading: IconButton(
             onPressed: () => context.pop(),
-            icon: SvgPicture.asset(AppIcons.svgsBack)),
+            icon: SvgPicture.asset(getIt<CacheHelper>()
+                        .getString(key: CacheKeys.currentLanguage) ==
+                    'en' ?AppIcons.svgsBack : AppIcons.svgsBackRight)),
       ),
       body: Padding(
         padding: const EdgeInsets.all(30.0),
